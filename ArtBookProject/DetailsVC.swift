ec132b87d2e1b70cdebf4c5ab949c26f32d6a5f0
Cengizhan Tomak <cengizhantomak@gmail.com>
2022-08-24 01:27:52 +0300

    Save Button Gorunum Ayarlari

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -14,6 +14,7 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var artistText: UITextField!
     @IBOutlet weak var yearText: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
     
     var chosenPainting = ""
     var chosenPaintingId : UUID?
@@ -22,6 +23,8 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         super.viewDidLoad()
         
         if chosenPainting != "" {
+            
+            saveButton.isHidden = true
             
             //Core Data
             let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -63,6 +66,8 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
             }
             
         } else {
+            saveButton.isHidden = false
+            saveButton.isEnabled = false
             nameText.text = ""
             artistText.text = ""
             yearText.text = ""
@@ -87,6 +92,7 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
+        saveButton.isEnabled = true
         self.dismiss(animated: true, completion: nil)
     }
     
